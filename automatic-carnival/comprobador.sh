36f69047fdd16255ee2d67afa65f665f040b074b
Miguel Bayón <miguel.bayon@iessanandres.com>
2023-10-27 11:49:13 +0200

    Commit inicial

---
+++ automatic-carnival/comprobador.sh
@@ -0,0 +1,139 @@
+FILE=Api.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=a.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=b.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=config/config.bck
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=config/config.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=config.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Controller.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=c.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/config/values.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data01.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data02.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data03.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data04.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/init.txt
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/x.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data2/data01.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data2/data02.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data2/data03.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data2/data04.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data2/Test.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=input.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Main.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=passwords.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Screen10.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Screen1.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Screen2.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Screen3.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=User.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
